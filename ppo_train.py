1ec2921efa83aa13b5779a682ceb881f4bc0c01f
loque18 <madridx2010@gmail.com>
2026-05-15 14:13:50 -0500

    update

--- ppo_train.py
+++ ppo_train.py
@@ -75,7 +75,7 @@ def train():
     # TRAIN
     # ==================================================
     model.learn(
-        total_timesteps=1_000_000,
+        total_timesteps=500_000,
         callback=[checkpoint_callback, eval_callback],
         progress_bar=True
     )
